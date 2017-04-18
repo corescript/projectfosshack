11aa8a80bd3a7f9108dd930d307b1bfbf4ed272b
Nadish Shajahan <nadishshahjahan@gmail.com>
2017-04-18 22:44:07 +0530

    AD

--- newsvibe/news/views.py
+++ newsvibe/news/views.py
@@ -7,6 +7,10 @@ from django.http import HttpResponse
 # Create your views here.
 import unirest
 
+def index(request):
+	
+    return render(request,'index.html',context=None)
+
 def get_news(request):
 	
 
