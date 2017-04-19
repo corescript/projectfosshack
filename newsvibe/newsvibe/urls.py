0aaef9861578536e2ef5464596fcce3ad017847a
user <user@user>
2017-04-19 10:18:59 +0530

    registration

--- newsvibe/newsvibe/urls.py
+++ newsvibe/newsvibe/urls.py
@@ -13,14 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
 from analyzer.views import *
 from news.views import *
+from .views import *
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^get/', get_news),
     url(r'^article/(?P<id>\d+)/$', article_view),
+    url(r'^home/$', home, name='home'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),  # <--
+        url(r'^settings/$', settings, name='settings'),
+    url(r'^settings/password/$', password, name='password'),
+
     url(r'^', index),
     
 ]
